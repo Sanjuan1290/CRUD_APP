14d000b5232cd58f487f5ecf99bf0bd0585e46dc
robert <robertrenbysanjuan@gmail.com>
2025-07-16 18:46:12 +0800

    add: AddRecord function

---
+++ src/components/AddRecord.jsx
@@ -0,0 +1,64 @@
+import crossIcon from '../assets/cross.png'
+
+export default function AddRecord({ showAddStudent, setShowAddStudent }){
+
+    async function addRecord(e){
+        e.preventDefault();
+
+        const formData = new FormData(e.target)
+        const f_name = formData.get('f_name')
+        const l_name = formData.get('l_name')
+        const age = formData.get('age')
+        
+        try{
+            const response = await fetch('http://localhost:3000/api/v1/add', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({f_name, l_name, age})
+            })
+
+            if(!response.ok) throw new Error('something went wrong');
+            setShowAddStudent(false);
+            e.target.reset()
+        }catch(err){
+            console.log(err);
+        }
+
+    }
+
+
+    return(
+        <section className={`fixed inset-0 bg-[rgba(0,0,0,0.43)] ${showAddStudent ? 'flex' : 'hidden'} items-start justify-center pt-20`}>
+            
+            <div className='bg-white w-[500px] rounded-sm'>
+
+                <div className='flex justify-between border-b pb-3 p-4 bg-gray-50'>
+                    <h1 className='text-[20px] font-semibold'>ADD STUDENTS</h1>
+                    <button onClick={()=>{setShowAddStudent(false)}}><img src={crossIcon} alt="close button" className='w-6'/></button>
+                </div>
+                
+                <form onSubmit={addRecord}>
+                    <div className='flex flex-col gap-1 px-4'>
+                        <label htmlFor="f_name" className='mt-2'>First Name</label>
+                        <input type="text" name='f_name' className='outline-cyan-500 border border-gray-300 rounded-md px-3 py-[5px]'/>
+
+                        <label htmlFor="l_name" className='mt-2'>Last Name</label>
+                        <input type="text" name='l_name' className='outline-cyan-500 border border-gray-300 rounded-md px-3 py-[5px]'/>
+
+                        <label htmlFor="age" className='mt-2'>Age</label>
+                        <input type="text" name='age' className='outline-cyan-500 border border-gray-300 rounded-md px-3 py-[5px]'/>
+                    </div>
+
+
+                    <div className='flex justify-end border-t mt-5 px-4 py-5 gap-2 bg-gray-50'>
+                        <button type='button' onClick={()=>{setShowAddStudent(false)}} className='bg-[rgb(95,102,109)] text-gray-50 py-2 px-4 rounded-md transition-colors duration-200 ease-in-out hover:bg-[rgb(73,79,85)]'>Close</button>
+                        <button className='bg-[rgb(32,145,58)] text-gray-50 py-2 px-4 rounded-md transition-colors duration-200 ease-in-out hover:bg-[rgb(26,121,48)]'>ADD</button>
+                    </div>
+
+                </form>
+            </div>
+        </section>
+    )
+}
--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import AddRecord from './AddRecord'
 
 export default function Students(){
     const fields = ['ID', 'First Name', 'Last Name', 'Age', 'Update', 'Delete']
     const [records, setRecords] = useState([{}])
+    const [showAddStudent, setShowAddStudent] = useState(false)
 
     useEffect(()=>{
         getStudentData()
@@ -28,7 +30,7 @@ export default function Students(){
 
             <div className="flex justify-between items-center">
                 <h1 className="text-[25px] md:text-[45px]">ALL STUDENT</h1>
-                <button className="text-[16px] md:text-[24px] whitespace-nowrap bg-[rgb(1,123,253)] px-5 py-2 rounded-md text-white transition-colors duration-300 ease-in-out hover:bg-[rgb(13,86,164)]">ADD STUDENTS</button>
+                <button onClick={()=>{setShowAddStudent(true)}} className="text-[16px] md:text-[24px] whitespace-nowrap bg-[rgb(1,123,253)] px-5 py-2 rounded-md text-white transition-colors duration-300 ease-in-out hover:bg-[rgb(13,86,164)]">ADD STUDENTS</button>
             </div>
 
             <div className="grid grid-cols-[1fr_1fr_1fr_1fr_1fr_1fr] md:grid-cols-[100px_1fr_1fr_100px_1fr_1fr] border border-gray-300 mt-5">
@@ -41,17 +43,18 @@ export default function Students(){
                 {
                     records.map((record, index) => (
                         <React.Fragment key={index}>
-                            <div className="border border-gray-300 p-2 font-semibold">{record.id}</div>
-                            <div className="border border-gray-300 p-2 font-semibold">{record.first_name}</div>
-                            <div className="border border-gray-300 p-2 font-semibold">{record.last_name}</div>
-                            <div className="border border-gray-300 p-2 font-semibold">{record.age}</div>
-                            <div className="border border-gray-300 p-2 font-semibold"><button className="bg-[rgb(37,166,68)] px-3 py-1 text-white rounded-[3px] leading-6 hover:bg-[rgb(31,136,55)] transition-colors duration-200 ease-in-out">Update</button></div>
-                            <div className="border border-gray-300 p-2 font-semibold"><button className="bg-[rgb(218,53,64)] px-3 py-1 text-white rounded-[3px] leading-6 hover:bg-[rgb(184,43,53)] transition-colors duration-200 ease-in-out">Delete</button></div>
+                            <div className={`border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-whtie'} border-gray-300 p-2 font-semibold`}>{record.id}</div>
+                            <div className={`border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-whtie'} border-gray-300 p-2 font-semibold`}>{record.first_name}</div>
+                            <div className={`border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-whtie'} border-gray-300 p-2 font-semibold`}>{record.last_name}</div>
+                            <div className={`border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-whtie'} border-gray-300 p-2 font-semibold`}>{record.age}</div>
+                            <div className={`border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-whtie'} border-gray-300 p-2 font-semibold`}><button className="bg-[rgb(37,166,68)] px-3 py-1 text-white rounded-[3px] leading-6 hover:bg-[rgb(31,136,55)] transition-colors duration-200 ease-in-out">Update</button></div>
+                            <div className={`border ${index % 2 === 0 ? 'bg-gray-200' : 'bg-whtie'} border-gray-300 p-2 font-semibold`}><button className="bg-[rgb(218,53,64)] px-3 py-1 text-white rounded-[3px] leading-6 hover:bg-[rgb(184,43,53)] transition-colors duration-200 ease-in-out">Delete</button></div>
                         </React.Fragment>
                     ))
                 }
             </div>
             
+            <AddRecord showAddStudent={showAddStudent} setShowAddStudent={setShowAddStudent}/>
         </main>
     )
 }
